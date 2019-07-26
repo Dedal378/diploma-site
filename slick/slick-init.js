9b8d64ad216cc29c581721c6c11065355b86167f
Dedal378 <orlovetsfoto@gmail.com>
2019-07-26 03:06:33 +0300

    Анимация компонентов, создание pop-up окон, слайдер на slick.

---
+++ slick/slick-init.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        // setting-name: setting-value
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+    });
+});
