beb645c4346e5fa87fc03390360c01f3657cb1d5
Dedal378 <orlovetsfoto@gmail.com>
2019-08-04 22:54:33 +0300

    Адаптивность, валидация форм, px > rem, pixelPerfect

--- slick/slick-init.js
+++ slick/slick-init.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
     $('.slider').slick({
-        // setting-name: setting-value
         slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 2000,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 3,
+                    dots: true
+                }
+            }
+        ]
     });
 });
